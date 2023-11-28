4845d90111d2a4fa8d8a9c03cfd62708bb2c56f3
Oguz Pastirmaci <oguz.pastirmaci@oracle.com>
2023-11-28 09:07:58 -0800

    Update ol7_a100.sh

--- terraform/rdma/cloud-init/ol7_a100.sh
+++ terraform/rdma/cloud-init/ol7_a100.sh
@@ -6,17 +6,11 @@ OKE_KUBELET_CA_CERT=$(curl -sH "Authorization: Bearer Oracle" -L http://169.254.
 
 # Adjust boot volume size        
 sudo dd iflag=direct if=/dev/oracleoci/oraclevda of=/dev/null count=1
-echo "1" | sudo tee /sys/class/block/`readlink /dev/oracleoci/oraclevda | cut -d'/' -f 2`/device/resca
+echo "1" | sudo tee /sys/class/block/`readlink /dev/oracleoci/oraclevda | cut -d'/' -f 2`/device/rescan
 sudo /usr/libexec/oci-growfs -y
 
 timedatectl set-timezone $${worker_timezone}
 
-# Initialize OKE
-# Do not remove the "oci.oraclecloud.com/oci-rdma-health-check" taint. It's used to wait until the RDMA network is configured on the node. The daemonset that you will deploy with the insructions will remove the taint when network is ready. 
-
 bash /etc/oke/oke-install.sh \
   --apiserver-endpoint $OKE_APISERVER_ENDPOINT \
-  --kubelet-ca-cert $OKE_KUBELET_CA_CERT \
-  --kubelet-extra-args "--register-with-taints=oci.oraclecloud.com/oci-rdma-health-check:NoSchedule"
-
-touch /var/log/oke.done
+  --kubelet-ca-cert $OKE_KUBELET_CA_CERT
