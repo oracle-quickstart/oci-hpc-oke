f03adc9697810f015553960c4e69507453395cde
Oguz Pastirmaci <oguzp@outlook.com>
2023-12-11 16:10:34 -0800

    Update Ubuntu cloud init

--- terraform/rdma/cloud-init/ubuntu_system.sh
+++ terraform/rdma/cloud-init/ubuntu_system.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Add OKE repo & install the package
-add-apt-repository -y 'deb [trusted=yes] https://objectstorage.us-phoenix-1.oraclecloud.com/p/ryJWdnkQSeI4ruDo9Jh77saOd5XTmORuzjv1k7GmxegExdR4atsUW2y4n7GWjkwq/n/hpc_limited_availability/b/oke_node_repo/o/ubuntu stable main'
+add-apt-repository -y 'deb [trusted=yes] https://idv4srl8nzr8.objectstorage.us-phoenix-1.oci.customer-oci.com/p/XBJ8n3MAcnlyAfSATU_4jYu4W-cB2TzqXtS8WOJk6XaGOROphH2OFlsSH_2aEabJ/n/idv4srl8nzr8/b/oke_node_repo/o/ubuntu stable main'
 
-apt install -y jq oci-oke-node-all=1.27.2*
+apt install -y oci-oke-node-all=1.27.2*
 
 oke bootstrap
