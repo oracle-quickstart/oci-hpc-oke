27d022eaf395ecda3a1e372bca148ae5e6f985c7
Oguz Pastirmaci <oguz.pastirmaci@oracle.com>
2024-07-22 15:07:05 -0700

    Update ubuntu.sh

--- terraform/cloud-init/ubuntu.sh
+++ terraform/cloud-init/ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Add OKE repo & install the package
-add-apt-repository -y ''deb [trusted=yes] https://objectstorage.us-ashburn-1.oraclecloud.com/p/1_NbjfnPPmyyklGibGM-qEpujw9jEpWSLa9mXEIUFCFYqqHdUh5cFAWbj870h-g0/n/hpc_limited_availability/b/oke_node_packages/o/1.29.1/ubuntu stable main'
+add-apt-repository -y 'deb [trusted=yes] https://objectstorage.us-ashburn-1.oraclecloud.com/p/1_NbjfnPPmyyklGibGM-qEpujw9jEpWSLa9mXEIUFCFYqqHdUh5cFAWbj870h-g0/n/hpc_limited_availability/b/oke_node_packages/o/1.29.1/ubuntu stable main'
 
 apt install -y oci-oke-node-all=1.29.1*
 
