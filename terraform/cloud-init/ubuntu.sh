3e7c92ffa8ba2152fc05399e4dd06fe522e641c8
OguzPastirmaci <oguz.pastirmaci@oracle.com>
2024-09-10 19:14:18 -0700

    Update package location

--- terraform/cloud-init/ubuntu.sh
+++ terraform/cloud-init/ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Add OKE repo & install the package
-add-apt-repository -y 'deb [trusted=yes] https://objectstorage.us-ashburn-1.oraclecloud.com/p/1_NbjfnPPmyyklGibGM-qEpujw9jEpWSLa9mXEIUFCFYqqHdUh5cFAWbj870h-g0/n/hpc_limited_availability/b/oke_node_packages/o/1.29.1/ubuntu stable main'
+add-apt-repository -y 'deb [trusted=yes] https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-jammy/kubernetes-1.29 stable main'
 
 apt install -y oci-oke-node-all=1.29.1*
 
