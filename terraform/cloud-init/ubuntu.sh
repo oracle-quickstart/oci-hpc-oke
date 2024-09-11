fe0aa13d32b7e344e0b75bf7d65c61671a6e2f09
OguzPastirmaci <oguz.pastirmaci@oracle.com>
2024-09-10 19:14:42 -0700

    Update package location

--- terraform/cloud-init/ubuntu.sh
+++ terraform/cloud-init/ubuntu.sh
@@ -3,6 +3,6 @@
 # Add OKE repo & install the package
 add-apt-repository -y 'deb [trusted=yes] https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-jammy/kubernetes-1.29 stable main'
 
-apt install -y oci-oke-node-all=1.29.1*
+apt install -y oci-oke-node-all*
 
 oke bootstrap --manage-gpu-services
