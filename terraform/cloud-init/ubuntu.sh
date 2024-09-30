8d48da6ca056c91a3b996e0a7bbf192019ed1a8f
Andrei Ilas <andrei.cva@gmail.com>
2024-09-30 19:58:31 +0300

    fix apt lock issue (#34)

--- terraform/cloud-init/ubuntu.sh
+++ terraform/cloud-init/ubuntu.sh
@@ -3,6 +3,14 @@
 # Add OKE repo & install the package
 add-apt-repository -y 'deb [trusted=yes] https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-jammy/kubernetes-1.29 stable main'
 
+while fuser /var/lib/apt/lists/lock >/dev/null 2>&1 ; do
+    echo "Waiting for other apt instances to exit"
+    # Sleep to avoid pegging a CPU core while polling this lock
+    sleep 1
+done
+
+apt update
+
 apt install -y oci-oke-node-all*
 
 oke bootstrap --manage-gpu-services
