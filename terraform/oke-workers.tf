2a580c14d3addbac82c36989c55a413a919fa871
Oguz Pastirmaci <oguz.pastirmaci@oracle.com>
2026-02-13 02:47:01 -0800

    Add per-pool kubernetes version, max pods, and node cycling (#128)

--- terraform/oke-workers.tf
+++ terraform/oke-workers.tf
@@ -66,45 +66,60 @@ locals {
 
   worker_pools = {
     "oke-system" = {
-      create           = local.create_workers
-      description      = "OKE-managed VM Node Pool for cluster operations and monitoring"
-      placement_ads    = [substr(var.worker_ops_ad, -1, 0)]
-      mode             = "node-pool"
-      size             = var.worker_ops_pool_size
-      shape            = var.worker_ops_shape
-      ocpus            = var.worker_ops_ocpus
-      memory           = var.worker_ops_memory
-      boot_volume_size = var.worker_ops_boot_volume_size
-      image_type       = "custom"
-      image_id         = local.worker_ops_image_id
-      cloud_init       = [{ content_type = "text/cloud-config", content = yamlencode(local.cloud_init) }]
+      create             = local.create_workers
+      description        = "OKE-managed VM Node Pool for cluster operations and monitoring"
+      placement_ads      = [substr(var.worker_ops_ad, -1, 0)]
+      mode               = "node-pool"
+      size               = var.worker_ops_pool_size
+      shape              = var.worker_ops_shape
+      ocpus              = var.worker_ops_ocpus
+      memory             = var.worker_ops_memory
+      boot_volume_size   = var.worker_ops_boot_volume_size
+      image_type         = "custom"
+      image_id           = local.worker_ops_image_id
+      max_pods_per_node  = var.worker_ops_max_pods_per_node
+      kubernetes_version           = coalesce(var.worker_ops_kubernetes_version, var.kubernetes_version)
+      node_cycling_enabled         = var.worker_ops_node_cycling_enabled
+      node_cycling_max_surge       = var.worker_ops_node_cycling_max_surge
+      node_cycling_max_unavailable = var.worker_ops_node_cycling_max_unavailable
+      cloud_init                   = [{ content_type = "text/cloud-config", content = yamlencode(local.cloud_init) }]
     }
     "oke-cpu" = {
-      create           = local.create_workers && var.worker_cpu_enabled
-      description      = "OKE-managed CPU Node Pool"
-      placement_ads    = [substr(var.worker_cpu_ad, -1, 0)]
-      mode             = "node-pool"
-      size             = var.worker_cpu_pool_size
-      shape            = var.worker_cpu_shape
-      ocpus            = lookup(local.worker_cpu_denseio_ocpus, var.worker_cpu_shape, var.worker_cpu_ocpus)
-      memory           = lookup(local.worker_cpu_denseio_memory, var.worker_cpu_shape, var.worker_cpu_memory)
-      boot_volume_size = var.worker_cpu_boot_volume_size
-      image_type       = "custom"
-      image_id         = local.worker_cpu_image_id
-      cloud_init       = [{ content_type = "text/cloud-config", content = yamlencode(local.cloud_init) }]
+      create             = local.create_workers && var.worker_cpu_enabled
+      description        = "OKE-managed CPU Node Pool"
+      placement_ads      = [substr(var.worker_cpu_ad, -1, 0)]
+      mode               = "node-pool"
+      size               = var.worker_cpu_pool_size
+      shape              = var.worker_cpu_shape
+      ocpus              = lookup(local.worker_cpu_denseio_ocpus, var.worker_cpu_shape, var.worker_cpu_ocpus)
+      memory             = lookup(local.worker_cpu_denseio_memory, var.worker_cpu_shape, var.worker_cpu_memory)
+      boot_volume_size   = var.worker_cpu_boot_volume_size
+      image_type         = "custom"
+      image_id           = local.worker_cpu_image_id
+      max_pods_per_node  = var.worker_cpu_max_pods_per_node
+      kubernetes_version           = coalesce(var.worker_cpu_kubernetes_version, var.kubernetes_version)
+      node_cycling_enabled         = var.worker_cpu_node_cycling_enabled
+      node_cycling_max_surge       = var.worker_cpu_node_cycling_max_surge
+      node_cycling_max_unavailable = var.worker_cpu_node_cycling_max_unavailable
+      cloud_init                   = [{ content_type = "text/cloud-config", content = yamlencode(local.cloud_init) }]
     }
     "oke-gpu" = {
-      create           = local.create_workers && var.worker_gpu_enabled
-      description      = "OKE-managed GPU Node Pool"
-      placement_ads    = [substr(var.worker_gpu_ad, -1, 0)]
-      mode             = "node-pool"
-      size             = var.worker_gpu_pool_size
-      shape            = var.worker_gpu_shape
-      boot_volume_size = var.worker_gpu_boot_volume_size
-      image_type       = "custom"
-      image_id         = local.worker_gpu_image_id
-      node_labels      = { "oci.oraclecloud.com/disable-gpu-device-plugin" : var.disable_gpu_device_plugin ? "true" : "false" },
-      cloud_init       = [{ content_type = "text/cloud-config", content = yamlencode(local.cloud_init) }]
+      create             = local.create_workers && var.worker_gpu_enabled
+      description        = "OKE-managed GPU Node Pool"
+      placement_ads      = [substr(var.worker_gpu_ad, -1, 0)]
+      mode               = "node-pool"
+      size               = var.worker_gpu_pool_size
+      shape              = var.worker_gpu_shape
+      boot_volume_size   = var.worker_gpu_boot_volume_size
+      image_type         = "custom"
+      image_id           = local.worker_gpu_image_id
+      max_pods_per_node  = var.worker_gpu_max_pods_per_node
+      kubernetes_version = coalesce(var.worker_gpu_kubernetes_version, var.kubernetes_version)
+      node_labels                  = { "oci.oraclecloud.com/disable-gpu-device-plugin" : var.disable_gpu_device_plugin ? "true" : "false" },
+      node_cycling_enabled         = var.worker_gpu_node_cycling_enabled
+      node_cycling_max_surge       = var.worker_gpu_node_cycling_max_surge
+      node_cycling_max_unavailable = var.worker_gpu_node_cycling_max_unavailable
+      cloud_init                   = [{ content_type = "text/cloud-config", content = yamlencode(local.cloud_init) }]
     }
     "oke-rdma" = {
       create                  = local.create_workers && var.worker_rdma_enabled && !local.invalid_worker_rdma_image
@@ -117,6 +132,8 @@ locals {
       boot_volume_vpus_per_gb = var.worker_rdma_boot_volume_vpus_per_gb
       image_type              = "custom"
       image_id                = local.worker_rdma_image_id
+      max_pods_per_node       = var.worker_rdma_max_pods_per_node
+      kubernetes_version      = coalesce(var.worker_rdma_kubernetes_version, var.kubernetes_version)
       cloud_init              = [{ content_type = "text/cloud-config", content = yamlencode(local.cloud_init) }]
       node_labels             = { "oci.oraclecloud.com/disable-gpu-device-plugin" : var.disable_gpu_device_plugin ? "true" : "false" },
       agent_config = {
